77da7f2d4ffee1467ce97a82b2916a229ce8ac23
rosemarydaris <rosemarydaris3@gmail.com>
2025-04-12 09:18:50 +0530

    Initial commit

--- script.js
+++ script.js
@@ -119,7 +119,21 @@ console.log(double)
 
 
 
+function calculate(num1, num2, operator) {
+    if (operator === '+') {
+        return num1 + num2;
+    } else if (operator === '-') {
+        return num1 - num2;
+    } else if (operator === '*') {
+        return num1 * num2;
+    } else if (operator === '/') {
+        return num1 / num2;
+    } else {
+        return 'Invalid operator';
+    }
+}
 
+console.log(calculate(5, 3, '+'));
 
 
 
